4196fc43f8cb8fbf3d938d5c06bd73010eb8e2f7
joshlarue <44442031+joshlarue@users.noreply.github.com>
2023-12-12 23:38:21 -0700

    added board

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
+from number import Number
 
-
-
+def boardSetup():
+    print('''
+    ___________________
+    |     |     |     |
+    |     |     |     |
+    |___ _|_____|_____|
+    |     |     |     |
+    |     |     |     |
+    |___ _|_____|_____|
+    |     |     |     |
+    |     |     |     |
+    |___ _|_____|_____|
+    ''')
 
 def main():
-    pass
+    num1 = Number()
+    num1.setPos(10, 20)
+    pos = num1.getPos()
+    print(pos)
+    numbers=[]
+    for i in range(10):
+        newNum = Number(i, i)
+        numbers.append(newNum)
+    for number in numbers:
+        print(number)
 
 
 if __name__ == '__main__':
--- number.py
+++ number.py
@@ -1,5 +1,15 @@
 class Number:
-    def __init__(self):
+    def __init__(self, x=0, y=0):
         self._number = 0
-        self.posX = 0
-        self.posY = 0
+        self._posX = x
+        self._posY = y
+
+    def getPos(self):
+        return ((self._posX, self._posY))
+
+    def setPos(self, x, y):
+        self._posX = x
+        self._posY = y
+
+    def __str__(self):
+        return f'{self._posX}, and {self._posY}'
